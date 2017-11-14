7f902a461c5bac3c8a75cae7c691d3c3f67f453e
Emaro <emaro+github@mailbox.org>
2017-11-14 10:31:12 +0100

    Add ComponentScan Annotation

--- src/main/java/controller/PageController.java
+++ src/main/java/controller/PageController.java
@@ -1,4 +1,20 @@
 package controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import persistence.Page;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+
+@RestController
+@RequestMapping("/page")
 public class PageController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    String pages() {
+        return "Hello im a Page";
+    }
 }
--- src/main/java/wdb/wdb/WdbApplication.java
+++ src/main/java/wdb/wdb/WdbApplication.java
@@ -2,8 +2,11 @@ package wdb.wdb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"controller"})
 public class WdbApplication {
 
 	public static void main(String[] args) {
