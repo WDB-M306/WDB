55ca0a59d6c18fb3b532389b2eba5941b3d244a5
Joel Häberli <haebu@rubigen.ch>
2017-11-14 09:04:34 +0100

    init 2

---
+++ src/main/java/wdb/wdb/WdbApplication.java
@@ -0,0 +1,12 @@
+package wdb.wdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WdbApplication.class, args);
+	}
+}
