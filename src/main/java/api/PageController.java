10387d955e3e054e349ed5c88f47ce2c36c08e60
Emaro <emaro+github@mailbox.org>
2017-11-14 11:18:22 +0100

    Add PageController dummy implementation

--- src/main/java/api/PageController.java
+++ src/main/java/api/PageController.java
@@ -1,17 +1,22 @@
 package api;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 
 @RestController
 @RequestMapping("/page")
-public class PageController {
-
+public class PageController extends Controller{
+    
+    public PageController (EntityManager em) {
+        super(em);
+    }
+    
     @RequestMapping(method = GET)
     String pages() {
         // return all pages
@@ -21,5 +26,39 @@ public class PageController {
     @RequestMapping(method = GET, value = "/{pageId}")
     String getPage(@PathVariable String pageId) {
         return "Id: " + pageId;
+    }
+    
+    @RequestMapping(method = POST)
+    String postPage(@RequestBody Page page) {
+        return "Created page «"+page.title+"»";
+    }
+    
+    @RequestMapping(method = POST, value = "/{pageId}/tag")
+    String addTagToPage(@PathVariable String pageId, @RequestBody long tagId) {
+        return String.format("Add tag with id %d to page with id %s", tagId, pageId);
+    }
+    
+    @RequestMapping(method = POST, value = "/{pageId}/attachement")
+    String addTagToPage(@PathVariable String pageId, @RequestBody Attachement attachement) {
+        return String.format("Add attachement to page with id %s, %s", pageId, attachement.toString());
+    }
+}
+
+@Entity
+class Page {
+    
+    public String title;
+    public String content;
+}
+
+@Entity
+class Attachement {
+    
+    public String filename;
+    public String content;
+    
+    @Override
+    public String toString () {
+        return String.format("Filename: %s\nContent: %s", filename, content);
     }
 }
