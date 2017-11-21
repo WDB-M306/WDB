3f4327be45a2c396bc4413ea3fda986070c93cd3
a <anuraly.keller@iet-gibb.ch>
2017-11-21 08:55:14 +0100

    add: Tag model

--- src/main/java/persistence/Tag.java
+++ src/main/java/persistence/Tag.java
@@ -1,4 +1,35 @@
 package persistence;
 
+import javax.persistence.*;
+
+@Entity
+@NamedQueries({
+        @NamedQuery(name = "Tag.findTagById", query = "SELECT t FROM Tag AS t WHERE t.id = ?1"),
+        @NamedQuery(name= "Tag.findTagByname", query = "SELECT t FROM Tag AS t WHERE t.name = ?1")
+})
 public class Tag {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private long id;
+    private string name
+    private string entries
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public string getName() {
+        return name;
+    }
+    public void setName(string name) {
+        this.name = name;
+    }
+    public string getEntries() {
+        return entries;
+    }
+    public void setEntries(string entries) {
+        this.entries = entries;
+    }
 }
